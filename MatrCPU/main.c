db36b6df4318e98ec161f5f1366b2b053fbe52e5
PKotik <PersonKotik@gmail.com>
2024-11-26 16:54:00 +0300

    Add files via upload

--- MatrCPU/main.c
+++ MatrCPU/main.c
@@ -3,6 +3,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
+#include <time.h>
+
 #define EPS 0.000001 
 
 
@@ -11,7 +13,7 @@ double** CreateMatrix(unsigned int n)
     srand(time(NULL));
     double** matrix = (double**)malloc(n * sizeof(double*));
     for (int i = 0; i < n; i++) {
-        matrix[i] = (double*)malloc(/*2**/n * sizeof(double));
+        matrix[i] = (double*)malloc(2*n * sizeof(double));
     }
 
     return matrix;
@@ -29,8 +31,8 @@ void freeMatrix(unsigned int n, double** matrix)
 void Inic(double** matrix, unsigned int n)
 {
     for (int i = 0; i < n; i++) {
-        for (int j = 0; j </*2**/n; j++) {
-            if (j < n) matrix[i][j] = 10 + rand() % 90;
+        for (int j = 0; j <2*n; j++) {
+            if (j < n) matrix[i][j] = rand() % 99999;
             else
             {
                 if (i + n == j) matrix[i][j] = 1;
@@ -40,24 +42,97 @@ void Inic(double** matrix, unsigned int n)
     }
 }
 
+void SRowMatrix(unsigned int row, double** matrix, unsigned int n)
+{
+    for (int i=0; i < n; i++)
+    {
+        if (i != row)
+        {
+            double k = matrix[i][row] / matrix[row][row];
+            for (int j = 0; j < 2 * n; j++)
+            {
+                matrix[i][j] -= k * matrix[row][j];
+            }
+        }
+    }
+}
 
-void PrintMatrix(double** matrix, unsigned int n)
+void SAllMatrix(double** matrix, unsigned int n)
+{
+    for (int z = 0; z < n; z++)
+    {
+        SRowMatrix(z, matrix, n);
+        //if (z % 50 == 0)
+        //{
+        //    system("cls");
+        //    puts("Wait.");
+        //}
+        //if (z % 101 == 0)
+        //{
+        //    system("cls");
+        //    puts("Wait..");
+        //}
+        //if (z % 151 == 0)
+        //{
+        //    system("cls");
+        //    puts("Wait...");
+        //}
+
+        
+    }
+}
+
+
+void PrintMatrixRev(double** matrix, unsigned int n)
 {
     for (int i = 0; i < n; i++) {
-        for (int j = 0; j < /*2 **/ n; j++) {
-            printf("%.6f ", matrix[i][j]);
+        for (int j = n; j < 2 * n; j++) {
+            printf("%.6f", matrix[i][j]);
         }
         printf("\n");
     }
 }
 
+void PrintMatrixOrig(double** matrix, unsigned int n)
+{
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%d ", (int)matrix[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void GoToOneRows(double** matrix, unsigned int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        double k = matrix[i][i];
+        matrix[i][i] /= k;
+        for (int j = n; j < 2 * n; j++)
+        {
+            matrix[i][j] /= k;
+        }
+    }
+}
+
 int main(void)
 {
-    double** M = CreateMatrix(3);
-    Inic(M, 3);
-    PrintMatrix(M, 3);
-    double** A= CreateMatrix(3);
-    PrintMatrix(A, 3);
-    freeMatrix(3,M);
-    freeMatrix(3, A);
+    int n = 1000;
+    double** M = CreateMatrix(n);
+    Inic(M, n);
+    clock_t start, end;
+    start = clock();
+    //PrintMatrixOrig(M, 1000);
+    SAllMatrix(M, n);
+    GoToOneRows(M, n);
+    end = clock();
+    freeMatrix(n, M);
+    double time = (((double)(end-start))*1000) / CLOCKS_PER_SEC;
+
+    /*PrintMatrixRev(M, 1000);*/
+    /*system("cls");*/
+    printf("Ready!!! Process took: ");
+    printf("%d ms", (int)time);
+    
 }
